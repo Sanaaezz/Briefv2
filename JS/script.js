49a246202a2908777202889745db48de1fcd3816
Sanaa <sanaa.ezzouine.simplon@gmail.com>
2024-01-30 19:31:31 +0100

    30janvier

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,16 @@
-window.alert(" Attention ce site est encore imparfait!");
+// window.alert(" Attention ce site est encore en chantier!");
+
+// let tryIt = document.querySelector(".tryIt");
+// tryIt.addEventListener("click", tryAgain);
+
+// function tryAgain() {
+//   tryIt = window.alert(" et bin non ! ");
+// }
 
 let tryIt = document.querySelector(".tryIt");
-tryIt = window.alert(" et bin non ! ");
+let imageV = document.querySelector(".visageTriste");
+
+window.afficher = function () {
+  imageV.style.display = imageV.style.display == "block" ? "none" : "block";
+};
+tryIt.onclick = afficher;
